106184373663d905e5960071ab61b54aa1f37319
Eris Lietus <yukitoichijo@gmail.com>
2025-08-21 16:03:16 +0100

    made the window work and open

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+# Asteroids Game
+# Dependencies: pygame 2.6.1
+# To install: uv add pygame==2.6.1
+import pygame
+from constants import SCREEN_HEIGHT, SCREEN_WIDTH, ASTEROID_MIN_RADIUS ,ASTEROID_KINDS, ASTEROID_SPAWN_RATE, ASTEROID_MAX_RADIUS
+# From the file constants I am importing the variables I have not used * (wildcard) to make it more readable. 
+
 def main():
-    print("Hello from asteroids!")
-
-
+    print(f"Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen,(0,0,0))
+        pygame.display.flip()
 if __name__ == "__main__":
     main()
